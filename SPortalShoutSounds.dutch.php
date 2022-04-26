6c4f05475239240063a5fafb85fd2fc8a002cc13
Felix <fparmentier@gmail.com>
2022-04-26 09:56:23 +0200

    Fixed caps in filename, added some translations

--- SPortalShoutSounds.dutch.php
+++ SPortalShoutSounds.dutch.php
@@ -1,20 +1,20 @@
-﻿<?php
-// Version: 1.34; EhPortal
+<?php
+// Version: 1.34; //Needs translation EhPortal
 // Based on Dutch translation for Simple Portal 2.3
-// Last revision: 2022-04-19 by MellotronScratch
+// Last revision: 2022-04-19 by MellotronScratch 
 
 /* Admin menu */
 
 // headings
 $txt['sp_sbSound_title_main'] = 'Shoutbox Audio - Mp3 File List'; //Needs translation
-$txt['sp_sbSound_title_upload'] = 'Upload Mp3 Files'; //Needs translation
-$txt['sp_sbSound_title_file'] = 'File Setting'; //Needs translation
-$txt['sp_sbSound_title_current'] = 'Current Settings'; //Needs translation
-$txt['sp_sbSound_name'] = 'File Name'; //Needs translation
-$txt['sp_sbSound_size'] = 'Size'; //Needs translation
-$txt['sp_sbSound_date'] = 'Date'; //Needs translation
-$txt['sp_sbSound_del'] = 'Delete'; //Needs translation
-$txt['sp_sbSound_sel'] = 'Select'; //Needs translation
+$txt['sp_sbSound_title_upload'] = 'Upload Mp3 Bestanden';
+$txt['sp_sbSound_title_file'] = 'Bestandsinstellingen';
+$txt['sp_sbSound_title_current'] = 'Huidige instellingen';
+$txt['sp_sbSound_name'] = 'Bestandsnaam';
+$txt['sp_sbSound_size'] = 'Grootte';
+$txt['sp_sbSound_date'] = 'Datum';
+$txt['sp_sbSound_del'] = 'Verwijder';
+$txt['sp_sbSound_sel'] = 'Selecteer';
 
 // main
 $txt['sp_admin_shoutbox_audio'] = 'Shoutbox Audio'; //Needs translation
@@ -23,8 +23,8 @@ $txt['sp_sbSoundEnable'] = 'Enable Shoutbox Sound Effect'; //Needs translation
 $txt['sp_sbSoundIntervalReset'] = '[Auto] Set every shoutbox refresh time to 10 seconds'; //Needs translation
 $txt['sp_sbSound'] = 'Choose Shoutbox Sound Effect'; //Needs translation
 $txt['sp_admin_shoutbox_audioTypes'] = '[Supported File Types: MP3]'; //Needs translation
-$txt['sp_admin_shoutbox_audioMax'] = 'Max File Size: 40 KB'; //Needs translation
-$txt['sp_admin_shoutbox_audioStop'] = 'Stop'; //Needs translation
+$txt['sp_admin_shoutbox_audioMax'] = 'Maximale bestandsgrootte: 40 KB';
+$txt['sp_admin_shoutbox_audioStop'] = 'Stop';
 $txt['sp_admin_shoutbox_audioSubmit'] = 'Submit'; //Needs translation
 $txt['sp_admin_shoutbox_audioFileSubmit'] = 'Change MP3'; //Needs translation
 $txt['sp_admin_shoutbox_audioFileDeleteAllOn'] = 'Check All'; //Needs translation
