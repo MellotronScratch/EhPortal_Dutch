854d1a12059c065f0185b30b037fd856af6700d7
Felix <fparmentier@gmail.com>
2023-08-10 11:09:25 +0200

    Added new attributes

--- SPortalShoutSounds.dutch.php
+++ SPortalShoutSounds.dutch.php
@@ -1,21 +1,21 @@
 <?php
-// Version: 1.39; EhPortal
+// Version: 1.39.5; EhPortal
 // Based on Dutch translation for Simple Portal 2.3
 // https://github.com/MellotronScratch/EhPortal_Dutch/
-// Last revision: 2022-12-20 by MellotronScratch
+// Last revision: 2023-08-10 by MellotronScratch
 
 /* Admin menu */
 
 // headings
 $txt['sp_sbSound_title_main'] = 'Shoutbox Audio - Mp3 File List'; //Needs translation
-$txt['sp_sbSound_title_upload'] = 'Upload Mp3 Bestanden';
-$txt['sp_sbSound_title_file'] = 'Bestandsinstellingen';
-$txt['sp_sbSound_title_current'] = 'Huidige instellingen';
-$txt['sp_sbSound_name'] = 'Bestandsnaam';
-$txt['sp_sbSound_size'] = 'Grootte';
-$txt['sp_sbSound_date'] = 'Datum';
-$txt['sp_sbSound_del'] = 'Verwijder';
-$txt['sp_sbSound_sel'] = 'Selecteer';
+$txt['sp_sbSound_title_upload'] = 'Upload Mp3 Files'; //Needs translation
+$txt['sp_sbSound_title_file'] = 'File Setting'; //Needs translation
+$txt['sp_sbSound_title_current'] = 'Current Settings'; //Needs translation
+$txt['sp_sbSound_name'] = 'File Name'; //Needs translation
+$txt['sp_sbSound_size'] = 'Size'; //Needs translation
+$txt['sp_sbSound_date'] = 'Date'; //Needs translation
+$txt['sp_sbSound_del'] = 'Delete'; //Needs translation
+$txt['sp_sbSound_sel'] = 'Select'; //Needs translation
 
 // main
 $txt['sp_admin_shoutbox_audio'] = 'Shoutbox Audio'; //Needs translation
@@ -24,8 +24,8 @@ $txt['sp_sbSoundEnable'] = 'Enable Shoutbox Sound Effect'; //Needs translation
 $txt['sp_sbSoundIntervalReset'] = '[Auto] Set every shoutbox refresh time to 10 seconds'; //Needs translation
 $txt['sp_sbSound'] = 'Choose Shoutbox Sound Effect'; //Needs translation
 $txt['sp_admin_shoutbox_audioTypes'] = '[Supported File Types: MP3]'; //Needs translation
-$txt['sp_admin_shoutbox_audioMax'] = 'Maximale bestandsgrootte: 40 KB';
-$txt['sp_admin_shoutbox_audioStop'] = 'Stop';
+$txt['sp_admin_shoutbox_audioMax'] = 'Max File Size: 40 KB'; //Needs translation
+$txt['sp_admin_shoutbox_audioStop'] = 'Stop'; //Needs translation
 $txt['sp_admin_shoutbox_audioSubmit'] = 'Submit'; //Needs translation
 $txt['sp_admin_shoutbox_audioFileSubmit'] = 'Change MP3'; //Needs translation
 $txt['sp_admin_shoutbox_audioFileDeleteAllOn'] = 'Check All'; //Needs translation
@@ -39,7 +39,7 @@ $txt['sp_admin_shoutbox_audioDelCurrent'] = 'You should not delete the current m
 // confirmation
 $txt['sp_admin_shoutbox_audioConfirm'] = 'Are you sure you want to do the following actions?'; //Needs translation
 $txt['sp_admin_shoutbox_audioConfirmActions'] = '[ %s ]'; //Needs translation
-$txt['sp_admin_shoutbox_audioConfirmDivider'] = ' & '; //Needs translation
+$txt['sp_admin_shoutbox_audioConfirmDivider'] = '&'; //Needs translation
 $txt['sp_admin_shoutbox_audioConfirmDel'] = 'file deletions'; //Needs translation
 $txt['sp_admin_shoutbox_audioConfirmChange'] = 'file changes'; //Needs translation
 $txt['sp_admin_shoutbox_audioConfirmFiles'] = 'upload files'; //Needs translation
@@ -50,7 +50,7 @@ $txt['sp_admin_shoutbox_audioConfirmSetRefresh'] = 'adjust refresh rates'; //Nee
 $txt['sp_admin_shoutbox_audioMessage'] = 'For this plug-in to function properly, you must set each shoutbox auto refresh setting to a minimum of 5 seconds (10 is recommended).'; //Needs translation
 $txt['sp_admin_shoutbox_audioDetect'] = '%s shoutboxes with less than a 5 second refresh rate have been detected.'; //Needs translation
 $txt['sp_admin_shoutbox_audioAuto'] = 'Select the [Auto] checkbox to set each shoutbox to a 10 second recommended delay time.'; //Needs translation
-$txt['sp_admin_shoutbox_audioMessageDivider'] = ' ~ '; //Needs translation
+$txt['sp_admin_shoutbox_audioMessageDivider'] = '~'; //Needs translation
 
 // errors
 $txt['sp_admin_shoutbox_audioLogFile'] = 'Error can\'t rename mp3 file to: %s'; //Needs translation
